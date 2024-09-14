edd3200951463ad6a431d542061991a6d8665797
swornim.shah <appdelivery@f1soft.com>
2024-09-14 15:19:45 +0545

    add type alias for parameters

---
+++ Sources/JunkeriFoundation/Dtos/RequestParams.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by swornim-shah on 14/09/2024.
+//
+
+import Foundation
+
+typealias AnyParams = [String: Any]
+typealias ApiTypeParams<T: Codable> = [String: T]
